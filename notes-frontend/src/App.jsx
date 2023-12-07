253b642d47c6daa3f1abb7e090fd9abe344a2660
derek williams <derek61@gmail.com>
2023-12-07 14:46:48 -0700

    Add invalidateQueries so that the UI is updated after a mutation

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -1,12 +1,46 @@
-import { useQuery } from '@tanstack/react-query'
-import { getNotes } from './requests' 
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote } from './requests'
 
 const App = () => {
+
+  const queryClient = useQueryClient()
+
+  /**
+   * Note: The fullstackopen docs uses both 
+   *    queryClient.invalidateQueries({ queryKey: ['notes'] }) 
+   *  and 
+   *    queryClient.invalidateQueries('notes)
+   * 
+   * See: https://fullstackopen.com/en/part6/react_query_use_reducer_and_the_context#synchronizing-data-to-the-server-using-react-query
+   * 
+   * The Tanstack documetation always uses the full object, e.g. { queryKey: ['notes'] }
+   * See: https://tanstack.com/query/v4/docs/react/guides/query-invalidation
+   * 
+   * Two calls are made when a mutation is run, the POST to create a note and a GET to retrive all of the notes
+   * Open the Browswer inspector to see this happen
+   * 
+   * From the FullStackOpen tutorial: 
+   * 
+   * "This in turn causes React Query to automatically 
+   * update a query with the key notes, i.e. fetch the notes 
+   * from the server. As a result, the application renders 
+   * the up-to-date state on the server, i.e. the added note 
+   * is also rendered."
+   */
+
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    newNoteMutation.mutate({ content, important: true })
   }
 
   const toggleImportance = (note) => {
@@ -18,16 +52,15 @@ const App = () => {
     queryFn: getNotes
   })
 
-  console.log(JSON.parse(JSON.stringify(result)))
+//  console.log(JSON.parse(JSON.stringify(result)))
 
-  if ( result.isLoading ) {
+  if (result.isLoading) {
     return <div>loading data...</div>
   }
 
   const notes = result.data
 
-
-  return(
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
@@ -36,7 +69,7 @@ const App = () => {
       </form>
       {notes.map(note =>
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
+          {note.content}
           <strong> {note.important ? 'important' : ''}</strong>
         </li>
       )}
